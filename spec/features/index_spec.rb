f7730805d2a9837ff96e6851175109952d0eb166
Jade Stewart <jadekstewart3@gmail.com>
2023-04-07 10:48:18 -0600

    Complete feature testing

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -4,11 +4,22 @@ RSpec.describe "Food Search Index" do
   describe "As a visitor" do 
     before :each do 
       visit root_path
-      save_and_open_page
     end
-
+    
     it "displays a form to search for foods" do
       expect(page).to have_field(:q)
+      
+      fill_in :q, with: "Sweet Potato"
+      
+      click_button "Search"
+      
+    
+      expect(current_path).to eq("/foods")
+
+      expect(page).to have_content("SWEET POTATO")
+      expect(page).to have_content("Upc:20042431")
+      expect(page).to have_content("Brand:FRESH & EASY")
+      expect(page).to have_content("Ingredients:SWEET POTATO, CHIPOTLE CHICKEN")
     end
   end
 end
